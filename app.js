5335b327e7113119daab6a706bb979fa258dfacb
Dominik Meier <d.meier@nextindex.de>
2018-03-03 12:27:46 +0100

    Add app.js, index.html, main.css
    new Functions in app.js
    Window Object

--- app.js
+++ app.js
@@ -1,24 +1,61 @@
 /*
+
+    TODO: 
+
     Definiere die anzahl der Bilder in der
     anzahlBilder Variable. 
 
+    Gib den Bildern eine überschrifft mit der createHeadline
+    Funktion.
 */
 
 var anzahlBilder = 8;
 
- function createPictures(i){
+/*
+    Macht die Bilder sichtbar.
+*/
+ function createPictures(i) {
     var getPictureArea = document.getElementById('picture');
     var newPicture = document.createElement('img');
-    var source =  "/Bilder/"+ i + ".jpg";
-    newPicture.src = source; 
+    var source =  "Bilder/"+ i + ".jpg";
+    newPicture.src = source;
     getPictureArea.appendChild(newPicture);
-    
+    newPicture.onclick = function NewWindow() {
+        window.open(source);
+        
+    };
+   
  }
 
-
+/*
+ * For schleife führt die Funktion createPictures i mal aus.
+ */
 
 for (var i = 1; i < anzahlBilder + 1; i++){
     createPictures(i);
+}
+
+/*
+ * Gibt  deinen Bildern eine Überschrift.
+ */
+
+function createHeadline(headline) {
+    var getHeadlineArea = document.getElementById('headline');
+    var writeHeadline = document.createElement('h1');
+    writeHeadline.innerHTML = headline;
+    getHeadlineArea.appendChild(writeHeadline);
+}
+
+/*
+ * Überschrift in " " eintragen.
+ */
+createHeadline("Beispiel Headline");
+
+/*
+ * Wenn der Button gedrückt ist Drucke die Seite aus.
+ */
+function printWindow() {
+    window.print();
 }
 
 
